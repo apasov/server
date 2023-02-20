750a5a233082b0b997f7d90da4eac3b25121a4f8
Julius Härtl <jus@bitgrid.net>
2023-02-20 22:12:00 +0100

    tests: Adapt node related unit tests mocks to required root view
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/dav/tests/unit/Connector/Sabre/DirectoryTest.php
+++ apps/dav/tests/unit/Connector/Sabre/DirectoryTest.php
@@ -76,7 +76,6 @@ class TestViewDirectory extends \OC\Files\View {
  * @group DB
  */
 class DirectoryTest extends \Test\TestCase {
-
 	use UserTrait;
 
 	/** @var \OC\Files\View | \PHPUnit\Framework\MockObject\MockObject */
@@ -219,13 +218,13 @@ class DirectoryTest extends \Test\TestCase {
 
 		$this->view->expects($this->any())
 			->method('getRelativePath')
-			->willReturnCallback(function($path) {
+			->willReturnCallback(function ($path) {
 				return str_replace('/admin/files/', '', $path);
 			});
 
 		$this->view->expects($this->any())
 			->method('getAbsolutePath')
-			->willReturnCallback(function($path) {
+			->willReturnCallback(function ($path) {
 				return Filesystem::normalizePath('/admin/files' . $path);
 			});
 
